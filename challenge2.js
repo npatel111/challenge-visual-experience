25135ff295d5693c0b8041c5b38d3288115bc7eb
Niti Nrw <niti.patel@flatironschool.com>
2019-05-17 13:19:19 -0400

    click x button to remove overlay

--- challenge2.js
+++ challenge2.js
@@ -10,26 +10,7 @@ var sizes = $(".size").find(".selectable");
 var prodImg = $('.primary-image')[1]
 var prodImgLink = prodImg.src
 
-// style shape, position of div
-var div = document.createElement('div');
-div.setAttribute('id', 'bx-overlay');
-div.style.backgroundColor = "blue";
-div.style.width = "300px";
-div.style.height = "300px";
-div.style.display = "none";
-div.style.margin = "O auto";
-div.style.position = "fixed";
-div.style.top = "50%";
-div.style.left = "50%";
-div.style.transform = "translateX(-50%) translateY(-50%)";
-document.getElementsByTagName('body')[0].appendChild(div)
-
-
-//style content of div
-div.innerText = `Get this ${prodName} for ${discountPrice} with a 15% discount`;
-
-
-// style height
+// style transparent div
 var transparentDiv = document.createElement('div');
 transparentDiv.style.width =  '100vw'
 transparentDiv.style.height = '100vh';
@@ -42,29 +23,50 @@ transparentDiv.style.zIndex = '3';
 transparentDiv.style.opacity="0.5";
 document.getElementsByTagName('body')[0].appendChild(transparentDiv);
 
+// style overlay
+var div = document.createElement('div');
+div.setAttribute('id', 'bx-overlay');
+div.style.backgroundColor = "blue";
+div.style.width = "300px";
+div.style.height = "300px";
+div.style.display = "none";
+div.style.margin = "O auto";
+div.style.position = "fixed";
+div.style.top = "50%";
+div.style.left = "50%";
+div.style.transform = "translateX(-50%) translateY(-50%)";
+div.style.zIndex = "4";
+document.getElementsByTagName('body')[0].appendChild(div)
 
 
-// style buttons
+//style content of div
+div.innerText = `Get this ${prodName} for ${discountPrice} with a 15% discount`;
 
 
+// style x buttons
+var xBtn = document.createElement('img');
+xBtn.src = "https://image.flaticon.com/icons/svg/118/118741.svg";
+xBtn.setAttribute('id', 'bx-close');
+xBtn.style.maxWidth = "4em";
+div.appendChild(xBtn);
 
 
 function toggleOverlay() {
   console.log('running toggleOverlay function');
   var overlay = document.getElementById('bx-overlay');
   if (overlay.style.display === "none") {
+    console.log('overlay was display none, now is display block');
     overlay.style.display = "block";
     transparentDiv.style.display = "block";
   } else {
     overlay.style.display = "none";
+    console.log('overlay was display block');
     transparentDiv.style.display = "none";
   }
 }
 
-// $(document).on('click', '.size .selectable', function() {alert('hi')})
-$(document).on('click', '.size', function() {toggleOverlay()})
+$(document).on('click', '.size', function() {toggleOverlay()});
+$(document).on('click', '#bx-close', function() {toggleOverlay()});
 
 
-
-// addClickListener();
 
